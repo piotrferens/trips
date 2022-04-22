fd2a7ae36394364eb7c45cfe3604c16cc547e601
Piotr Ferens <piotrferens@outlook.com>
2022-04-22 12:23:54 +0200

    Remove container

--- src/ui/pages/home/home.tsx
+++ src/ui/pages/home/home.tsx
@@ -1,9 +1,8 @@
 import { Typography } from 'ui/atoms';
-import { Container } from 'ui/template';
 import { TripList } from 'ui/organisms';
 
 import { HomeProps } from './home.types';
 
 export const Home = ({ trips }: HomeProps) => (
-  <Container>{trips ? <TripList trips={trips} /> : <Typography el="h1">Something went wrong</Typography>}</Container>
+  <div>{trips ? <TripList trips={trips} /> : <Typography el="h1">Something went wrong</Typography>}</div>
 );
